b027bf90bc1af61699c5ee1f1c9fa388ce614532
Brian Woodward <brian.woodward@gmail.com>
2015-08-05 17:34:54 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * clone-repos <https://github.com/doowb/clone-repos>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
